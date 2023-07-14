f420373d451c0be216316e5c92fe69926dcdca95
Ihliana <Ihnatco@protonmail.com>
2023-07-14 08:26:30 +0200

    refactoring unlike a tweet

--- index.js
+++ index.js
@@ -25,11 +25,14 @@ function handleLikeClick(tweetId){
 
      if(targetTweetObj.isLiked){
             targetTweetObj.likes--
-            targetTweetObj.isLiked = false
+          
+        
         } else {
             targetTweetObj.likes++
-            targetTweetObj.isLiked = true
+         
         }
+
+        targetTweetObj.isLiked = !targetTweetObj.isLiked
 
    render()
 
